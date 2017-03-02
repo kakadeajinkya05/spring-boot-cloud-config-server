d97296996a51272fb8d3228cf6bf650fd01a2459
ajinkya <ajinkya_kakade@gap.com>
2017-03-03 03:04:28 +0530

    first commit

---
+++ src/main/java/com/configServer/SpringBootCloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.configServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class SpringBootCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloudConfigServerApplication.class, args);
+	}
+}
