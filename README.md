d5a307ddc792d8785fbf64fc552d9b6fe1c4688d
kakadeajinkya05@gmail.com <kakadeajinkya05@gmail.com>
2017-12-10 23:09:23 +0530

    [Ajinkya: Disables eureka server for now and updated to latest release]

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring-boot-cloud-config-server
+
+	## Check properties from git repos 
+		
+		```
+		http://localhost:8888/reservation/default
+		``` 
--- src/main/java/com/configServer/SpringBootCloudConfigServerApplication.java
+++ src/main/java/com/configServer/SpringBootCloudConfigServerApplication.java
@@ -1,15 +1,17 @@
 package com.configServer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class SpringBootCloudConfigServerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootCloudConfigServerApplication.class, args);
 	}
